dbaa0d7be08133df73d68a42052265c20dad378b
JIHWAN <jjjh0508@gmail.com>
2023-08-03 20:56:39 +0900

    테스트 코드 작성

--- src/test/java/com/jihwan/study/OrderTests.java
+++ src/test/java/com/jihwan/study/OrderTests.java
@@ -1,5 +1,115 @@
 package com.jihwan.study;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Date;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderTests {
-    
+    private static EntityManagerFactory managerFactory;
+    private EntityManager entityManager;
+
+
+    @BeforeAll
+    public static void initFactory() {
+        managerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = managerFactory.createEntityManager();
+
+    }
+
+
+    @AfterAll
+    public static void closeFactory() {
+        managerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+
+    @Test
+    @Order(1)
+    void 제품_등록_테스트() {
+
+        //given
+        Product product = new Product();
+        product.setProductName("갤럭시 폴드");
+        product.setProductPrice(2000000);
+
+
+        //when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+        entityManager.persist(product);
+        try {
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+            e.printStackTrace();
+        }
+
+        //then
+    Product findProduct1 = entityManager.find(Product.class,product.getProductNum());
+
+    Assertions.assertEquals(product,findProduct1);
+
+    }
+
+    @Test
+    @Order(2)
+    void 제품_조회_테스트() {
+        Product findProduct1 = entityManager.find(Product.class,1);
+        System.out.println(findProduct1);
+        Assertions.assertNotNull(findProduct1);
+    }
+
+    @Test
+    @Order(3)
+    void 주문_등록_테스트() {
+        com.jihwan.study.Order order = new com.jihwan.study.Order();
+        order.setStatus("Y");
+        order.setOrderDate(new Date());
+
+        entityManager.persist(order);
+        Product findProduct1 = entityManager.find(Product.class,1);
+        OrderMapping orderMapping = new OrderMapping();
+        orderMapping.setOrderPk(new OrderPk(order.getOrderNum(),findProduct1.getProductNum()));
+
+        entityManager.persist(orderMapping);
+
+        Assertions.assertEquals(order.getOrderNum(),orderMapping.getOrderPk().getOrderNum());
+
+    }
+
+
+    @Test
+    @Order(4)
+    void 주문_삭제_테스트() {
+        //given
+        com.jihwan.study.Order order = entityManager.find(com.jihwan.study.Order.class,1);
+        //when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.remove(order);
+        entityTransaction.commit();
+
+        //then
+
+        com.jihwan.study.Order  findOrder = entityManager.find(com.jihwan.study.Order.class,order.getOrderNum());
+        Assertions.assertNull(findOrder);
+
+
+    }
 }
